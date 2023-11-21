86591f89f0df6ae8e31af4fbdf20796e1412a57f
HsM <85266091+iHsMDev@users.noreply.github.com>
2023-11-21 10:55:26 +0300

    Empty

--- src/Components/Cart/CartItems.tsx
+++ src/Components/Cart/CartItems.tsx
@@ -20,41 +20,44 @@ const CartItems = ({ Items }: { Items: any }) => {
   };
   return (
     <div className={styles.items}>
-      <motion.header
-        variants={ani}
-        initial="hidden"
-        animate="animate"
-        custom={0}
-        className={styles.dataHeader}
-      >
-        <h4 className={styles.storeName}>صورة المنتج</h4>
-        <div className={styles.dataAll}>
-          <motion.p
-            variants={ani}
-            initial="hidden"
-            animate="animate"
-            custom={1}
-          >
-            اسم المنتج
-          </motion.p>
-          <motion.p
-            variants={ani}
-            initial="hidden"
-            animate="animate"
-            custom={2}
-          >
-            سعر المنتج
-          </motion.p>
-          <motion.p
-            variants={ani}
-            initial="hidden"
-            animate="animate"
-            custom={3}
-          >
-            الكمية
-          </motion.p>
-        </div>
-      </motion.header>
+      {Items.length != 0 && (
+        <motion.header
+          variants={ani}
+          initial="hidden"
+          animate="animate"
+          custom={0}
+          className={styles.dataHeader}
+        >
+          <h4 className={styles.storeName}>صورة المنتج</h4>
+          <div className={styles.dataAll}>
+            <motion.p
+              variants={ani}
+              initial="hidden"
+              animate="animate"
+              custom={1}
+            >
+              اسم المنتج
+            </motion.p>
+            <motion.p
+              variants={ani}
+              initial="hidden"
+              animate="animate"
+              custom={2}
+            >
+              سعر المنتج
+            </motion.p>
+            <motion.p
+              variants={ani}
+              initial="hidden"
+              animate="animate"
+              custom={3}
+            >
+              الكمية
+            </motion.p>
+          </div>
+        </motion.header>
+      )}
+
       {Items.length >= 1 ? (
         Items.map((item: { index: number; count: number }, index: number) => (
           <Item
