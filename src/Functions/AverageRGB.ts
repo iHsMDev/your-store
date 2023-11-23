aee7f7efc755873bc278f5c2466f337ffa159429
HsM <85266091+iHsMDev@users.noreply.github.com>
2023-11-23 16:49:31 +0300

    Profile

--- src/Functions/AverageRGB.ts
+++ src/Functions/AverageRGB.ts
@@ -1,17 +1,19 @@
 export const AverageColor = (imgSrc: any) => {
-  console.log(imgSrc);
-
   var canvas: any = document.createElement("canvas");
   let ctx = canvas.getContext("2d");
   let img = new Image();
   img.setAttribute("crossOrigin", "");
   img.src = imgSrc;
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("");
-
-      ctx.drawImage(img, 0, 0, 1, 1);
-    }, 200);
+    const haha = setInterval(() => {
+      if (img) {
+        setTimeout(() => {
+          resolve("");
+          ctx.drawImage(img, 0, 0, 1, 1);
+          clearInterval(haha);
+        }, 500);
+      }
+    }, 500);
   }).then(() => {
     return ctx.getImageData(0, 0, 1, 1).data.slice(0, 3);
   });
